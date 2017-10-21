32a320b1e9101ae44fe14461be56f1dc832d442d
Võ Tiến Khoa <tienkhoa2803.hcmus@gmail.com>
2017-10-21 12:17:41 +0700

    feature_addfriend_v1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: [:index, :show, :new, :create, :update, :destroy]
   resources :users, only: [:index, :show]
+  resources :friendships, only:[:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
